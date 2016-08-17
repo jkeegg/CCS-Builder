e240bb2875b7a88b7c8f266207f6fa8938a26a15
liuc <kvs112219@gmail.com>
2016-08-17 15:38:06 +0800

    CCS Builder首次提交
    Signed-off-by: liuc <kvs112219@gmail.com>

---
+++ CCS Builder/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace CCS_Builder
+{
+    partial class FormMainWindow
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMainWindow));
+            this.BuildButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxProjectPath = new System.Windows.Forms.TextBox();
+            this.SelectButton = new System.Windows.Forms.Button();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.backgroundWorkerExec = new System.ComponentModel.BackgroundWorker();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.buttonHelp = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BuildButton
+            // 
+            this.BuildButton.Location = new System.Drawing.Point(137, 56);
+            this.BuildButton.Name = "BuildButton";
+            this.BuildButton.Size = new System.Drawing.Size(106, 23);
+            this.BuildButton.TabIndex = 0;
+            this.BuildButton.Text = "Build";
+            this.BuildButton.UseVisualStyleBackColor = true;
+            this.BuildButton.Click += new System.EventHandler(this.BuildButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ProjectPath";
+            // 
+            // textBoxProjectPath
+            // 
+            this.textBoxProjectPath.AllowDrop = true;
+            this.textBoxProjectPath.Location = new System.Drawing.Point(14, 29);
+            this.textBoxProjectPath.Name = "textBoxProjectPath";
+            this.textBoxProjectPath.Size = new System.Drawing.Size(229, 21);
+            this.textBoxProjectPath.TabIndex = 2;
+            this.textBoxProjectPath.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBoxProjectPath_DragDrop);
+            this.textBoxProjectPath.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBoxProjectPath_DragEnter);
+            // 
+            // SelectButton
+            // 
+            this.SelectButton.Location = new System.Drawing.Point(12, 56);
+            this.SelectButton.Name = "SelectButton";
+            this.SelectButton.Size = new System.Drawing.Size(109, 23);
+            this.SelectButton.TabIndex = 3;
+            this.SelectButton.Text = "Select";
+            this.SelectButton.UseVisualStyleBackColor = true;
+            this.SelectButton.Click += new System.EventHandler(this.SelectButton_Click);
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(12, 85);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ReadOnly = true;
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxLog.Size = new System.Drawing.Size(231, 258);
+            this.textBoxLog.TabIndex = 4;
+            // 
+            // backgroundWorkerExec
+            // 
+            this.backgroundWorkerExec.WorkerReportsProgress = true;
+            this.backgroundWorkerExec.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerExec_DoWork);
+            this.backgroundWorkerExec.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorkerExec_ProgressChanged);
+            this.backgroundWorkerExec.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorkerExec_RunWorkerCompleted);
+            // 
+            // buttonHelp
+            // 
+            this.buttonHelp.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonHelp.Location = new System.Drawing.Point(168, 3);
+            this.buttonHelp.Name = "buttonHelp";
+            this.buttonHelp.Size = new System.Drawing.Size(75, 23);
+            this.buttonHelp.TabIndex = 5;
+            this.buttonHelp.Text = "Help";
+            this.buttonHelp.UseVisualStyleBackColor = true;
+            this.buttonHelp.Click += new System.EventHandler(this.buttonHelp_Click);
+            // 
+            // FormMainWindow
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(255, 355);
+            this.Controls.Add(this.buttonHelp);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.SelectButton);
+            this.Controls.Add(this.textBoxProjectPath);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BuildButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormMainWindow";
+            this.Text = "CCS Builder";
+            this.TopMost = true;
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBoxProjectPath_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBoxProjectPath_DragEnter);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BuildButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxProjectPath;
+        private System.Windows.Forms.Button SelectButton;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerExec;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Button buttonHelp;
+    }
+}
+
---
+++ CCS Builder/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+
+
+namespace CCS_Builder
+{
+    public partial class FormMainWindow : Form
+    {
+        public FormMainWindow()
+        {
+            InitializeComponent();
+            this.textBoxProjectPath.Text = Settings1.Default.last_workspace;
+            this.textBoxProjectPath.SelectionStart = this.textBoxProjectPath.TextLength; 
+            this.textBoxProjectPath.ScrollToCaret();
+        }
+
+        private void BuildButton_Click(object sender, EventArgs e)
+        {
+            Settings1.Default.last_workspace = this.textBoxProjectPath.Text;
+            Settings1.Default.Save();
+            string default_exe = Settings1.Default.eclipsec_path;
+            if (!File.Exists(default_exe) || (Control.ModifierKeys == Keys.Shift))
+            {
+                OpenFileDialog fileDialog = new OpenFileDialog();
+                fileDialog.Multiselect = false;
+                fileDialog.Filter = "可执行文件(*.exe)|*.exe";
+                fileDialog.Title = "请选择eclipsec.exe所在路径";
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    default_exe = fileDialog.FileName;
+                    Settings1.Default.eclipsec_path = default_exe;
+                    Settings1.Default.Save();
+                }
+                else
+                    return;
+            }
+            bwArgs args = new bwArgs();
+            args.ws_path = textBoxProjectPath.Text;
+            args.eclipsec_path = default_exe;
+            BuildButton.Enabled = false;
+            textBoxLog.Text = ">>>> Start call CCSv5 <<<<\r\n\r\n";
+            backgroundWorkerExec.RunWorkerAsync(args);
+        }
+
+        private void SelectButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FolderDialog = new FolderBrowserDialog();
+
+            if (FolderDialog.ShowDialog() == DialogResult.OK)
+            {
+                this.textBoxProjectPath.Text = FolderDialog.SelectedPath;
+                this.textBoxProjectPath.SelectionStart = this.textBoxProjectPath.TextLength;
+                this.textBoxProjectPath.ScrollToCaret();
+            }
+
+            //OpenFileDialog fileDialog = new OpenFileDialog();
+            //fileDialog.Multiselect = false;
+            //fileDialog.Title = "请选择工作路径";
+            //if (fileDialog.ShowDialog() == DialogResult.OK || fileDialog.ShowDialog() == DialogResult.Yes)
+            //{
+            //    this.textBoxProjectPath.Text = fileDialog.FileName;
+            //}
+           
+        }
+
+        private void textBoxProjectPath_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void textBoxProjectPath_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                if (Path.GetExtension(file) == "")  //判断文件类型，只接受txt文件
+                {
+                    textBoxProjectPath.Text = file;
+                    this.textBoxProjectPath.SelectionStart = this.textBoxProjectPath.TextLength;
+                    this.textBoxProjectPath.ScrollToCaret();
+                    return;
+                }
+            }
+            backgroundWorkerExec.ReportProgress(0, "Directory only!");
+        }
+
+        private void p_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            backgroundWorkerExec.ReportProgress(0, e.Data);
+        }
+
+        private void backgroundWorkerExec_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                Process p = new Process();
+                p.StartInfo.FileName = (e.Argument as bwArgs).eclipsec_path;
+                string tmp;
+                string ws;
+                string pj;
+                string metadata;
+                string path = (e.Argument as bwArgs).ws_path;
+                if (path.LastIndexOf('\\') == -1)
+                {
+                    string msg;
+                    msg = string.Format("Invalid Path: {0}", path);
+                    backgroundWorkerExec.ReportProgress(0, msg);
+                    //MessageBox.Show(msg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                ws = path.Substring(0, path.LastIndexOf('\\'));
+                pj = path.Substring(path.LastIndexOf('\\'));
+                pj = pj.Replace("\\", "");
+                if (ws == "" || pj == "")
+                {
+                    string msg;
+                    msg = string.Format("Invalid ProjectPath\r\nWorkspace: {0}\r\nProjectName: {1}\r\n", ws, pj);
+                    backgroundWorkerExec.ReportProgress(0, msg);
+                    //MessageBox.Show(msg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                metadata = ws + "\\.metadata";
+                if (!Directory.Exists(metadata))
+                {
+                    string msg;
+                    msg = string.Format("Workspace: {0}\r\n isn't a valid CCSv5 workspace",ws);
+                    backgroundWorkerExec.ReportProgress(0, msg);
+                    //MessageBox.Show(msg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                tmp = string.Format("-noSplash -data \"{0}\" -application com.ti.ccstudio.apps.projectBuild -ccs.configuration Release -ccs.buildType incremental -ccs.projects {1}", ws, pj);
+                //backgroundWorkerExec.ReportProgress(0, string.Format("Compile command:\r\n{0}\r\n\r\n", tmp));
+                p.StartInfo.Arguments = tmp;
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardInput = true;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                p.StartInfo.CreateNoWindow = true;
+                p.OutputDataReceived += new DataReceivedEventHandler(p_OutputDataReceived);
+                p.Start();
+                p.BeginOutputReadLine();
+
+                //p.StandardInput.WriteLine("ping 127.0.0.1 -n 3");
+
+                //p.StandardInput.WriteLine("exit");
+
+                //this.textBoxLog.Clear();
+                //string tmpStr = p.StandardOutput.ReadLine();
+                //while (tmpStr != null)
+                //{
+                //    this.textBoxLog.Text += tmpStr;
+                //    //Application.DoEvents();
+                //    tmpStr = p.StandardOutput.ReadLine();
+                //}
+                p.WaitForExit();
+                p.Close();
+            }
+            catch (Win32Exception)
+            {
+                string msg = "";
+                msg = string.Format("{0} 无法执行\r\n请选择正确的eclipsec.exe可执行文件\r\n", (e.Argument as bwArgs).eclipsec_path);
+                backgroundWorkerExec.ReportProgress(0, msg);
+                //MessageBox.Show(msg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception  ex)
+            { 
+                string msg = "";
+                msg = string.Format("执行失败\r\n{0}\r\n", ex);
+                backgroundWorkerExec.ReportProgress(0, msg);
+                //MessageBox.Show(msg, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void backgroundWorkerExec_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            textBoxLog.AppendText("\r\n\r\n>>>> Finish call CCSv5 <<<<\r\n");
+            BuildButton.Enabled = true;
+        }
+
+        private void backgroundWorkerExec_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.UserState != null)
+            {
+                this.textBoxLog.AppendText(e.UserState as string);
+                this.textBoxLog.AppendText("\r\n");
+            }
+        }
+
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(string.Format("使用方法:\r\n1.选择或拖放CCSv5 Workspace目录下的工程目录到程序窗口\r\n2.点击Build编译工程\r\n3.程序默认扫描CCSv5安装路径,若找不到eclipsec.exe,点击Build会弹窗供选择,按住Shift点击Build,可强制选择\r\n\r\n当前配置:\r\neclipsec path = {0}\r\n\r\n\r\n\r\nCopyright © LiuChen. All Rights Reserved.", Settings1.Default.eclipsec_path), "帮助", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+        }
+    }
+}
+
+
+class bwArgs
+{
+    public string eclipsec_path;
+    public string ws_path;
+}
